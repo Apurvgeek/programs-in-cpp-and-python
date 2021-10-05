6f9077b29e365370b35c0cac7586873d515ae4f3
Plavani Sahoo <78641611+plavani0208@users.noreply.github.com>
2021-10-05 20:53:18 +0530

    pwd_generator in python

---
+++ pwd_generator.py
@@ -0,0 +1,26 @@
+# A program in Python to generate random password based on user input
+
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+','@']
+
+print("Welcome to the Password Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+pass_list=[]
+
+for char in range (0,nr_letters):
+  pass_list += random.choice(letters)
+
+for sym in range (0, nr_symbols ):
+  pass_list+= random.choice(symbols)
+
+for num in range (0,nr_numbers):
+  pass_list+= random.choice(numbers)
+
+random.shuffle(pass_list)
+password="".join(map(str,pass_list))
+print(password)
